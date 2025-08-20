ffed13bf6838232b8302b144c79497bdeb19c0f3
carlwobhs <carl.wright@obhs.school.nz>
2025-08-20 22:19:07 +1200

    Inputs and a function added

--- heads_tales.py
+++ heads_tales.py
@@ -1,2 +1,14 @@
 import random
 
+def heads_tails():
+    pass
+
+
+
+
+#--main------------
+print('Welcome to Heads and Tails game')
+name = input('Enter your name: ')
+age = int(input('Enter your age: '))
+
+heads_tails()
